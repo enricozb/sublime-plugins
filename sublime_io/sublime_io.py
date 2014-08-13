af20ae6900b7b601417ce4823f5355aa84ac3b63
Michael Opara <mk_opara@outlook.com>
2014-08-13 15:13:33 -0500

    Update sublime_io.py

--- sublime_io/sublime_io.py
+++ sublime_io/sublime_io.py
@@ -4,11 +4,13 @@ import sublime_plugin
 import subprocess
 import time
 
-DOUBLE = {
-           'cols': [0.0, 0.5, 1.0],
-           'rows': [0.0, 1.0],
-           'cells': [[0, 0, 1, 1], [1, 0, 2, 1]]
-         }
+# refresh without jitter
+# figure out a way to move to end of file
+# append data instead of having a file
+
+DOUBLE = {'cols': [0.0, 0.5, 1.0],
+          'rows': [0.0, 1.0],
+          'cells': [[0, 0, 1, 1], [1, 0, 2, 1]]}
 
 class SublimeIo(sublime_plugin.WindowCommand):
     def init(self, cmd):
@@ -31,15 +33,15 @@ class SublimeIo(sublime_plugin.WindowCommand):
         self.output_view = self.window.open_file(self.output_path)
         
         self.output_view.set_scratch(True)
+        self.output_view.set_read_only(True)
         self.window.set_layout(DOUBLE)
         self.window.run_command('move_to_group', {'group': 1})
-        self.window.focus_view(self.view)
 
     def input_panel(self):
         input_panel = self.window.show_input_panel('',
                                                    '',
                                                    self.on_done,
-                                                   self.on_change,
+                                                   None,
                                                    self.on_cancel)
         input_panel.settings().set('color_scheme', self.scheme)
 
@@ -48,28 +50,31 @@ class SublimeIo(sublime_plugin.WindowCommand):
         self.proc.stdin.flush()
         self.input_panel()
 
-    def on_change(self, string):
-        self.output_view.run_command('revert')
-
     def on_cancel(self):
         self.proc.kill()
         self.output_view.close()
         self.window.set_layout(self.layout)
 
     def finish(self):
+        while self.proc.poll() is None:
+            self.output_view.run_command('revert')
+
+        code = self.proc.poll()
         elapsed = time.time() - self.start_time
-        code = self.proc.wait()
 
         if code:
-            string = '[Finished in %.1fs with exit code %d]' % (elapsed, code)
+            chars = '[Finished in %.1fs with exit code %d]' % (elapsed, code)
         else:
-            string = '[Finished in %.1fs]' % (elapsed)
-        open(self.output_path, 'a').write(string)
-        self.output_view.run_command('revert')
+            chars = '[Finished in %.1fs]' % (elapsed)
+        
         self.output_view.run_command('move_to', {'to': 'eof'})
+        self.window.show_input_panel('', '', None, None, None)
+        self.window.run_command('hide_panel')
+        self.output_view.run_command('append', {'characters':  chars})
 
     def run(self):
-        self.init('python C:/Users/Music/Desktop/script.py')
+        self.init('C:/Users/Music/Desktop/Python34/pythonw.exe')
         self.newlayout()
         self.input_panel()
         sublime.set_timeout_async(self.finish, 0)
+        self.window.focus_view(self.view)
