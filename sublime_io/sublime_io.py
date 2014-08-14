1df18c7140f4255e0397b13539ad78541580b721
Michael Opara <mk_opara@outlook.com>
2014-08-13 21:12:41 -0500

    Update sublime_io.py

--- sublime_io/sublime_io.py
+++ sublime_io/sublime_io.py
@@ -4,10 +4,6 @@ import sublime_plugin
 import subprocess
 import time
 
-# refresh without jitter
-# figure out a way to move to end of file
-# append data instead of having a file
-
 DOUBLE = {'cols': [0.0, 0.5, 1.0],
           'rows': [0.0, 1.0],
           'cells': [[0, 0, 1, 1], [1, 0, 2, 1]]}
@@ -16,26 +12,26 @@ class SublimeIo(sublime_plugin.WindowCommand):
     def init(self, cmd):
         self.view = self.window.active_view()
         self.scheme = self.view.settings().get('color_scheme')
-        self.output_path = os.path.join(sublime.packages_path(), '[output]')
-        self.output_file = open(self.output_path, 'w')
         self.layout = self.window.layout()
 
         self.proc = subprocess.Popen(args = cmd,
                                      bufsize = 0,
                                      stdin = subprocess.PIPE,
-                                     stdout = self.output_file,
+                                     stdout = subprocess.PIPE,
                                      stderr = subprocess.STDOUT,
                                      shell = True,
                                      universal_newlines = True)
         self.start_time = time.time()
 
     def newlayout(self):
-        self.output_view = self.window.open_file(self.output_path)
+        self.window.set_layout(DOUBLE)
+        self.window.focus_group(1)
         
+        self.output_view = self.window.new_file()
+        
+        self.output_view.set_name('[output]')
         self.output_view.set_scratch(True)
         self.output_view.set_read_only(True)
-        self.window.set_layout(DOUBLE)
-        self.window.run_command('move_to_group', {'group': 1})
 
     def input_panel(self):
         input_panel = self.window.show_input_panel('',
@@ -57,7 +53,12 @@ class SublimeIo(sublime_plugin.WindowCommand):
 
     def finish(self):
         while self.proc.poll() is None:
-            self.output_view.run_command('revert')
+            stdout = self.proc.stdout.read()
+            self.output_view.set_read_only(False)
+            self.output_view.run_command('append', {'characters': stdout,
+                                                    'force': True,
+                                                    'scroll_to_end': True})
+            self.output_view.set_read_only(True)
 
         code = self.proc.poll()
         elapsed = time.time() - self.start_time
@@ -68,12 +69,12 @@ class SublimeIo(sublime_plugin.WindowCommand):
             chars = '[Finished in %.1fs]' % (elapsed)
         
         self.output_view.run_command('move_to', {'to': 'eof'})
-        self.window.show_input_panel('', '', None, None, None)
-        self.window.run_command('hide_panel')
+        self.output_view.set_read_only(False)
         self.output_view.run_command('append', {'characters':  chars})
+        self.output_view.set_read_only(True)
 
     def run(self):
-        self.init('C:/Users/Music/Desktop/Python34/pythonw.exe')
+        self.init('C:/Users/Music/Desktop/script.exe')
         self.newlayout()
         self.input_panel()
         sublime.set_timeout_async(self.finish, 0)
