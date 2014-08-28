8cad4e895534bad7aef86aa38876a404dd6b5eef
Michael Opara <mk_opara@outlook.com>
2014-08-28 11:38:19 -0500

    Update subl.bat

--- subl/subl.bat
+++ subl/subl.bat
@@ -1,5 +1,19 @@
-IF %1 EQU "push" SET src = "C:/Users/michael.opara.1/%2" & SET dst = "H:/WORK/%2"
-IF %1 EQU "pull" SET src = "H:/WORK/%2" & SET dst = "C:/Users/michael.opara.1/%2"
-IF EXIST %dst% RD /S /Q %dst%
-IF EXIST %src% ROBOCOPY %src% %dst% /E 
+IF "%1" == "push" (
+    SET src="C:/Users/michael.opara.1/%2"
+    SET dst="H:/WORK/%2"
+)
+
+IF "%1" == "pull" (
+    SET src="H:/WORK/%2"
+    SET dst="C:/Users/michael.opara.1/%2"
+)
+
+IF EXIST "%dst%" (
+    RD /S /Q %dst%
+)
+
+IF EXIST "%src%" (
+    ROBOCOPY %src% %dst% /E 
+)
+
 PAUSE
