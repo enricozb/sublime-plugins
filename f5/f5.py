4762e2d77cbe728681c4a4fb06298240cfbc5fa4
Michael Opara <mk_opara@outlook.com>
2014-08-30 16:44:34 -0500

    Update f5.py

--- f5/f5.py
+++ f5/f5.py
@@ -1,36 +1,37 @@
+import functools
 import glob
 import os
 import sublime
 import sublime_plugin
 
-class F5(sublime_plugin.TextCommand):
-	def java(self, run):
-		cmd = 'javac "%(path)s"'
-		cmd = cmd + ' && java "%(base)s"' if run else cmd
+class F5Command(sublime_plugin.TextCommand):
+    def java(self, run):
+        return 'java "{bn}"' if run else 'javac "{fp}"' 
 
+    def cpp(self, run):
+        return '"{bn}"' if run else 'g++ "{fp}" -o "{bn}"' 
+    
+    def py(self, run):
+        return 'python -u "{fp}"'
 
-	def cpp(self, run):
-		cmd = 'g++ "%(path)s" -Wall -o "%(base)s"'
-		cmd = cmd + ' && "%(path)s"' if run else cmd
+    def pde(self, run):
+        #place in dir and return ''
+        pass
 
-		return ('make', {'cmd': cmd})
-	
-	def py(self, run):
-		cmd = 'python -u "%(path)s"'
+    def run(self, edit, run):
+        fp = self.view.file_name()
+        dp = os.path.dirname(fp)
+        bn = os.path.splitext(os.path.basename(fp))[0]
+        ex = os.path.splitext(os.path.basename(fp))[1].strip('.').lower()
+        sd = os.path.dirname(os.path.dirname(sublime.executable_path()))
+        
+        path = [dp, 'C:/Program Files*/Java/jdk*/bin', '{sd}/*/bin', '{sd}/Python*']
+        path = ';'.join(sum(map(glob.glob, path), []))
+        classpath = '.;'
 
-		return ('make', {'cmd': cmd})
+        cmd = getattr(self, ex, lambda run: '')(run).format(print(locals()) or locals())
 
-	def pde(self, run):
-		# place inside proper dir
-		return (('build'), {})
-
-	def run(self, run):
-		self.path = None
-		self.classpath = None
-
-		{path : self.view.file_name(),
-		 base : os.path.splitext(os.path.basename(path))[0],
-		 ext : os.path.splitext(os.path.basename(path))[1].strip('.'),
-		}
-		
-		self.view.window().run_command(*getattr(self, ext, lambda arg: 0)(run))
+        if cmd:
+            self.view.window().run_command('make', {'cmd': cmd, 'env': {'path': path, 'classpath': classpath}})
+        else:
+            self.view.window().run_command('build')
