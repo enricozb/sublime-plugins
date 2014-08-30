39864d8d6f62afb2d1e5f0fda5c5e0c0fd282b6f
Michael Opara <mk_opara@outlook.com>
2014-08-30 16:58:06 -0500

    Update make.py

--- make/make.py
+++ make/make.py
@@ -4,24 +4,26 @@ import sublime_plugin
 import subprocess
 import time
 
-# set for shell if not window along with syntax
+# set for shell if not windows along with syntax
 # figure out why python does not work
 
 class Process(object):
     def __init__(self, cmd, env):
-        proc_env = os.environ.copy()
-        proc_env.update(env)
+        env = dict(os.environ, **env)
         self.proc = subprocess.Popen(args = cmd, bufsize = 0, stdin = subprocess.PIPE,
-            stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True, env = proc_env)
+            stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell = True, env = env)
 
     def kill(self):
-        if sublime.platform() == "windows":
-            subprocess.call("taskkill /PID %d" % self.proc.pid, shell = True)
+        self.proc.stdin.close()
+        self.proc.stdout.close()
+
+        if sublime.platform() == 'windows':
+            subprocess.call('taskkill /PID %d' % self.proc.pid, shell = True)
         else:
             self.proc.terminate()
 
     def read(self):
-        return os.read(self.proc.stdout.fileno(), 2 ** 15).decode().replace("\r\n", "\n").replace("\r", "\n")
+        return os.read(self.proc.stdout.fileno(), 2 ** 15).decode().replace('\r\n', '\n').replace('\r', '\n')
 
     def write(self, string):
         os.write(self.proc.stdin.fileno(), string.encode())
@@ -34,22 +36,22 @@ class Make(sublime_plugin.WindowCommand):
         self.window = window
         self.view = self.window.active_view()
         self.layout = self.window.layout()
-        self.scheme = self.view.settings().get("color_scheme")
+        self.scheme = self.view.settings().get('color_scheme')
 
     def set_layout(self, name):
-        self.window.set_layout({"cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0],"cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
+        self.window.set_layout({'cols': [0.0, 0.5, 1.0], 'rows': [0.0, 1.0],'cells': [[0, 0, 1, 1], [1, 0, 2, 1]]})
         self.window.focus_group(1)
         self.window.new_file()
-        self.window.active_view().set_name("[%s]" % name)
+        self.window.active_view().set_name('[%s]' % name)
         self.window.active_view().set_scratch(True)
-        self.window.active_view().settings().set("scroll_past_end", False)
+        self.window.active_view().settings().set('scroll_past_end', False)
 
         self.output_view = self.window.active_view()
 
     def on_done(self, string):
-        self.proc.write(string + "\n")
-        self.output_view.run_command("append", {"characters": string + "\n"})
-        self.output_view.run_command("move_to", {"to": "eof"})
+        self.proc.write(string + '\n')
+        self.output_view.run_command('append', {'characters': string + '\n'})
+        self.output_view.run_command('move_to', {'to': 'eof'})
         self.input_panel(self.on_done, None, self.on_cancel)
 
     def on_cancel(self):
@@ -59,31 +61,28 @@ class Make(sublime_plugin.WindowCommand):
         self.window.focus_view(self.view)
 
     def input_panel(self, on_done, on_change, on_cancel):
-        panel = self.window.show_input_panel("", "", on_done, on_change, on_cancel)
+        panel = self.window.show_input_panel('', '', on_done, on_change, on_cancel)
         
-        panel.settings().set("color_scheme", self.scheme)
-        panel.settings().set("syntax", self.syntax)
+        panel.settings().set('color_scheme', self.scheme)
 
     def do(self):
         while self.proc.poll() is None:
-           self.output_view.run_command("append", {"characters":  self.proc.read()}) 
+           self.output_view.run_command('append', {'characters':  self.proc.read()}) 
 
         exitcode = self.proc.poll()
         elapsed = time.time() - self.start_time
 
         if exitcode:
-            string = "[Finished in %.1fs with exit code %d]" % (elapsed, exitcode)
+            string = '[Finished in %.1fs with exit code %d]' % (elapsed, exitcode)
         else:
-            string = "[Finished in %.1fs]" % elapsed
+            string = '[Finished in %.1fs]' % elapsed
 
-        self.output_view.run_command("append", {"characters":  string})
+        self.output_view.run_command('append', {'characters':  string})
 
-    def run(self, cmd, env):
-        self.name = "cmd" if cmd == "cmd" else self.view.file_name()
-        self.syntax = "Packages/Batch File/Batch File.tmLanguage" if cmd == "cmd" else self.view.settings().get("syntax")
+    def run(self, cmd, env = {}):
         self.proc = Process(cmd, env)
         self.start_time = time.time()
         
-        self.set_layout(os.path.basename(self.name))
+        self.set_layout(os.path.basename(self.view.file_name()))
         self.input_panel(self.on_done, None, self.on_cancel)
         sublime.set_timeout_async(self.do)
