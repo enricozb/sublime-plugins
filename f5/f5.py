d9b3a5b701b5ac824f1705459bd835177e5f1764
Michael Opara <mk_opara@outlook.com>
2014-08-31 10:25:57 -0500

    Update f5.py

--- f5/f5.py
+++ f5/f5.py
@@ -1,37 +1,38 @@
-import functools
 import glob
 import os
 import sublime
 import sublime_plugin
 
-class F5Command(sublime_plugin.TextCommand):
+class F5(sublime_plugin.TextCommand):
+    def __init__(self, view):
+        self.view = view
+        self.fn = os.path.basename(self.view.file_name() or '')
+        self.bn = os.path.splitext(self.fn)[0]
+        self.ex = os.path.splitext(self.fn)[1].strip('.').lower()
+        self.sd = os.path.dirname(os.path.dirname(sublime.executable_path()))
+
     def java(self, run):
-        return 'java "{bn}"' if run else 'javac "{fp}"' 
+        return 'java "{bn}"' if run else 'javac *.java'
 
     def cpp(self, run):
-        return '"{bn}"' if run else 'g++ "{fp}" -o "{bn}"' 
+        return '"{bn}"' if run else 'g++ "{fn}" -o "{bn}"' 
     
     def py(self, run):
-        return 'python -u "{fp}"'
+        return 'python -u "{fn}"'
 
     def pde(self, run):
         #place in dir and return ''
         pass
 
     def run(self, edit, run):
-        fp = self.view.file_name()
-        dp = os.path.dirname(fp)
-        bn = os.path.splitext(os.path.basename(fp))[0]
-        ex = os.path.splitext(os.path.basename(fp))[1].strip('.').lower()
-        sd = os.path.dirname(os.path.dirname(sublime.executable_path()))
-        
-        path = [dp, 'C:/Program Files*/Java/jdk*/bin', '{sd}/*/bin', '{sd}/Python*']
-        path = ';'.join(sum(map(glob.glob, path), []))
         classpath = '.;'
-
-        cmd = getattr(self, ex, lambda run: '')(run).format(**locals())
+        path = 'C:/Program Files*/Java/jdk*/bin;{sd}/*/bin;{sd}/Python*;'
+        path = map(glob.glob, path.format(**vars(self)).split(';'))
+        path = ';'.join(dir for dirs in path for dir in dirs)
+        cmd = getattr(self, self.ex, lambda run: '')(run).format(**vars(self))
 
         if cmd:
-            self.view.window().run_command('make', {'cmd': cmd, 'env': {'path': path, 'classpath': classpath}})
+            self.view.window().run_command('make', {'cmd': cmd,
+                'env': {'PATH': path, 'CLASSPATH': classpath}})
         else:
             self.view.window().run_command('build')
