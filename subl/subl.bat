6189d177d2d8c6d9ac7f9e673d9d1f70d6b7a3a3
Michael Opara <mk_opara@outlook.com>
2014-09-04 10:22:01 -0500

    Update subl.bat

--- subl/subl.bat
+++ subl/subl.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+SETLOCAL EnableDelayedExpansion
+
 IF %1 EQU push (
     SET src=C:/Users/michael.opara.1
     SET dst=H:/WORK
@@ -14,9 +16,9 @@ FOR /F %%G IN ('DIR /B %2') DO (
     SET src=%src%/%%G
     SET dst=%dst%/%%G
 
-    IF EXIST %src (
-        RD /S /Q %dst%
+    IF EXIST !src! (
+        RD /S /Q !dst!
     )
     
-    ROBOCOPY %src% %dst% /E 
+    ROBOCOPY !src! !dst! /E 
 )
