f5af7da8c86a6faf7156b7f46fd9939d5783afed
Michael Opara <mk_opara@outlook.com>
2014-09-12 10:43:49 -0500

    Update peep.py

--- peep/peep.py
+++ peep/peep.py
@@ -1,19 +1,22 @@
 import glob
-import os
+import os.path
 import sublime
 import sublime_plugin
 
-class Peep(sublime_plugin.TextCommand):
-    def run(self, edit):
-        files = (os.path.dirname(view.file_name()) for window in sublime.windows() for view in window.views())
-        files = sum((glob.glob(dirpath + '/*') for dirpath in files), [])
-        files = filter(os.path.isfile, set(files))
+class Peep(sublime_plugin.WindowCommand):
+    def on_select(self, files, i):
+        if i != -1:
+            self.window.open_file(files[i])
+
+    def run(self):
+        array = lambda f: [os.path.basename(f), f]
+        key = lambda array: array[1].lower()
+
+        files = (v.file_name() for w in sublime.windows() for v in w.views())
+        files = (os.path.dirname(f) for f in files if f)
+        files = (p for d in files for p in glob.iglob("%s/*" % d))
+        files = sorted(set(p for p in files if os.path.isfile(p)), key = key)
         
-        tolist = lambda file: [os.path.basename(file), file]
-        key = lambda list: list[0].lower()
+        items = list(array(f) for f in files)
 
-        items = sorted(map(tolist, files), key = key)
-        index = items.index(tolist(self.view.file_name()))
-        on_select = lambda i: self.view.window().open_file(items[index if i == -1 else i][1])
-
-        self.view.window().show_quick_panel(items, on_select, 0, index)
+        self.window.show_quick_panel(items, lambda i : self.on_select(files, i))
