ee43e372f70866c8816ebce74dfd76ddad889658
Michael Opara <mk_opara@outlook.com>
2014-09-15 11:37:47 -0500

    Update f5.py

--- f5/f5.py
+++ f5/f5.py
@@ -5,7 +5,7 @@ import sublime_plugin
 
 class F5(sublime_plugin.TextCommand):
     def java(self, run):
-        return 'java "{bn}"' if run else 'javac *.java'
+        return 'java "{bn}"' if run else 'javac "{fp}"'
 
     def cpp(self, run):
         return '"{bn}"' if run else 'g++ "{fn}" -o "{bn}"' 
@@ -13,26 +13,29 @@ class F5(sublime_plugin.TextCommand):
     def py(self, run):
         return 'python -u "{fn}"'
 
+    def js(self, run):
+        return 'java "{bn}"' if run else 'java org.mozilla.javascript.tools.jsc.Main "{fp}"'
+
     def pde(self, run):
-        # make sure dir is correct and return ''
+        # make sure dir is correct and return ""
         pass
 
     def run(self, edit, run):
-        fp = self.view.file_name() or ''
+        fp = self.view.file_name() or ""
         dp = os.path.dirname(fp)
         fn = os.path.basename(fp)
         bn = os.path.splitext(fn)[0]
-        ex = os.path.splitext(fn)[1].strip('.').lower()
+        ex = os.path.splitext(fn)[1].strip(".").lower()
         sd = os.path.dirname(os.path.dirname(sublime.executable_path()))
 
-        classpath = '.;'
-        path = 'C:/Program Files*/Java/jdk*/bin;{sd}/*/bin;{sd}/Python*;{dp}'
-        path = map(glob.glob, path.format(**locals()).split(';'))
-        path = ';'.join(dir for dirs in path for dir in dirs)
-        cmd = getattr(self, ex, lambda run: '')(run).format(**locals())
+        classpath = ".;H:/WORK/rhino1_7R4"
+        path = "C:/Program Files*/Java/jdk*/bin;{sd}/*/bin;{sd}/Python*;{dp}"
+        path = map(glob.glob, path.format(**locals()).split(";"))
+        path = ";".join(dir for dirs in path for dir in dirs)
+        cmd = getattr(self, ex, lambda run: "")(run).format(**locals())
 
         if cmd:
-            self.view.window().run_command('subliminal', {'cmd': cmd, 'PATH': path,
-                'CLASSPATH': classpath})
+            self.view.window().run_command("subliminal", {"cmd": cmd, "PATH": path,
+                "CLASSPATH": classpath})
         else:
-            self.view.window().run_command('build')
+            self.view.window().run_command("build")
