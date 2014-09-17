404afb90bbb6ca7550fc949363734efa653e8fc4
Michael Opara <mk_opara@outlook.com>
2014-09-17 18:34:46 -0500

    Update f5.py

--- f5/f5.py
+++ f5/f5.py
@@ -8,7 +8,7 @@ class F5(sublime_plugin.TextCommand):
         return 'java "{bn}"' if run else 'javac "{fp}"'
 
     def cpp(self, run):
-        return '"{bn}"' if run else 'g++ "{fn}" -o "{bn}"' 
+        return '"{bn}"' if run else 'g++ "{fn}" -o "{bn}"'
     
     def py(self, run):
         return 'python -u "{fn}"'
@@ -21,18 +21,10 @@ class F5(sublime_plugin.TextCommand):
         pass
 
     def run(self, edit, run):
-        fp = self.view.file_name() or ""
-        dp = os.path.dirname(fp)
-        fn = os.path.basename(fp)
-        bn = os.path.splitext(fn)[0]
-        ex = os.path.splitext(fn)[1].strip(".").lower()
-        sd = os.path.dirname(os.path.dirname(sublime.executable_path()))
-
-        classpath = ".;H:/WORK/rhino1_7R4/js.jar"
+        ex = os.path.splitext(self.view.file_name() or "")[1].strip(".").lower()
         path = "C:/Program Files*/Java/jdk*/bin;{sd}/*/bin;{sd}/Python*;{dp}"
-        path = map(glob.glob, path.format(**locals()).split(";"))
-        path = ";".join(dir for dirs in path for dir in dirs)
-        cmd = getattr(self, ex, lambda run: "")(run).format(**locals())
+        classpath = ".;{st}/Data/Packages/User/*.jar"
+        cmd = getattr(self, ex, lambda run: "")(run)
 
         if cmd:
             self.view.window().run_command("subliminal", {"cmd": cmd, "PATH": path,
