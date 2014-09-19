659543ea12dadcbb65558c7e1f13b5013bc6e0cf
Michael Opara <mk_opara@outlook.com>
2014-09-19 08:50:40 -0500

    Update f5.py

--- f5/f5.py
+++ f5/f5.py
@@ -1,33 +1,24 @@
-import glob
-import os.path
+import re
 import sublime
 import sublime_plugin
 
 class F5(sublime_plugin.TextCommand):
-    def java(self, run):
-        return 'java "{bn}"' if run else 'javac "{fp}"'
-
-    def cpp(self, run):
-        return '"{bn}"' if run else 'g++ "{fn}" -o "{bn}"'
-    
-    def py(self, run):
-        return 'python -u "{fn}"'
-
-    def js(self, run):
-        return 'java "{bn}"' if run else 'java org.mozilla.javascript.tools.jsc.Main "{fp}"'
-
-    def pde(self, run):
-        # make sure dir is correct and return ""
-        pass
-
     def run(self, edit, run):
-        ex = os.path.splitext(self.view.file_name() or "")[1].strip(".").lower()
+        match = re.search(r"(?<=[.]).*?(?= )", self.view.scope_name(0))
+        lang = match and match.group()
+        cmd = {
+            "java": 'java "{bn}"' if run else 'javac "{fp}"',
+            "c++": '"{bn}"' if run else 'g++ "{fn}" -o "{bn}"',
+            "python": 'python -u "{fn}"',
+            "js": 'java "{bn}"' if run else 'java org.mozilla.javascript.tools.jsc.Main "{fp}"',
+        }.get(lang, "")
+
         path = "C:/Program Files*/Java/jdk*/bin;{sd}/*/bin;{sd}/Python*;{dp}"
         classpath = ".;{st}/Data/Packages/User/*.jar"
-        cmd = getattr(self, ex, lambda run: "")(run)
 
         if cmd:
             self.view.window().run_command("subliminal", {"cmd": cmd, "PATH": path,
                 "CLASSPATH": classpath})
         else:
             self.view.window().run_command("build")
+
